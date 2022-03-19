c1f750f10c4a62a1943a35b8810fc56d9c18c1db
Ubuntu <ubuntu@ip-172-31-95-30.ec2.internal>
2022-03-19 02:53:50 +0000

    user-list my

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,8 +17,8 @@ class MessagesController < ApplicationController
   
   
   def index
-    @pools = Pool.where(online: true)
     @pool = Pool.find_by(id: cookies.signed[:user_id])
+    @pools = Pool.where(online: true).or(Pool.where(id: @pool.id))
     
     if logged_in?
       @word = current_user.words.build
